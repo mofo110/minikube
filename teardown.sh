163d44ba84ec77487d80bc06e88821ab508fdbc0
morton <morton_fojas@yahoo.com>
2018-09-02 12:06:55 -0700

    Fix teardown.sh

--- teardown.sh
+++ teardown.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+eval $(minikube docker-env)
+
 kubectl delete service hello-svc
 kubectl delete deployment hello-svc
 
 kubectl delete service mongo-db
 kubectl delete deployment mongo-db
 
-eval $(minikube docker-env)
 docker kill $(docker ps -q)
 docker rmi -f $(docker images -a -q)
 
